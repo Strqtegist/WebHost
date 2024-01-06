4536bf1e8a9f8e2381694c6b96a77110c0d43d61
Strqtegist <41564016+Strqtegist@users.noreply.github.com>
2024-01-06 16:58:15 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,22 +1,36 @@
 <?php
 
-// Validar que el usuario exista
-$usuarios = json_decode(file_get_contents("usuarios.json"));
-if (!array_key_exists($_POST['usuario'], $usuarios)) {
-  echo "El usuario no existe.";
-  exit;
+// Define la variable usuarios
+$usuarios = json_decode(fread($archivo, filesize("usuarios.json")));
+
+// Abre el archivo usuarios.json
+$archivo = fopen("usuarios.json", "r");
+
+// Lee el archivo
+$usuarios = json_decode(fread($archivo, filesize("usuarios.json")));
+
+// Cierra el archivo
+fclose($archivo);
+
+// Comprueba si el usuario existe
+if (!isset($usuarios[$_POST["usuario"]])) {
+
+  // El usuario no existe
+  echo "USUARIO NO EXISTE";
+
+} else {
+
+  // El usuario existe
+  if ($usuarios[$_POST["usuario"]]["contraseña"] == $_POST["contraseña"]) {
+
+    // La contraseña es correcta
+    header("Location: principal.html");
+  } else {
+
+    // La contraseña es incorrecta
+    echo "La contraseña es incorrecta";
+  }
+
 }
-
-// Validar que la contraseña sea correcta
-if ($usuarios[$_POST['usuario']]['contraseña'] != $_POST['contraseña']) {
-  echo "La contraseña es incorrecta.";
-  exit;
-}
-
-// Iniciar sesión
-session_start();
-$_SESSION['usuario'] = $_POST['usuario'];
-
-echo "Inicio de sesión exitoso.";
 
 ?>
